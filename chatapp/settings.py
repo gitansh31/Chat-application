cdf218fcf974be28cf836325fbcfcece2980b417
gitansh31 <gitansh31@gmail.com>
2021-01-14 21:05:38 +0530

    channel layer enabled

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -111,3 +111,11 @@ STATIC_URL = '/static/'
 
 # Channels
 ASGI_APPLICATION = 'chatapp.asgi.application'
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
